600c64cad26bf9a0e35264a7e3e6bacc48051f3b
starwook <star.wookk@gmail.com>
2022-12-14 18:58:41 +0900

    feat: Add getLineInformations method

--- src/main/java/subway/domain/LineInformationRepository.java
+++ src/main/java/subway/domain/LineInformationRepository.java
@@ -1,11 +1,15 @@
 package subway.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LineInformationRepository {
     private static final List<LineInformation> lineInformations= new ArrayList<>();
     public static void addLineInformation(LineInformation lineInformation){
         lineInformations.add(lineInformation);
+    }
+    public static List<LineInformation> getLineInformations(){
+        return Collections.unmodifiableList(lineInformations);
     }
 }
