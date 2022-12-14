c342fa6908130f3618a09177cb36b9e106919101
starwook <star.wookk@gmail.com>
2022-12-14 18:54:31 +0900

    feat: Add LineInformation 생성자

--- src/main/java/subway/domain/LineInformation.java
+++ src/main/java/subway/domain/LineInformation.java
@@ -1,10 +1,23 @@
 package subway.domain;
 
+import java.util.List;
+
 public class LineInformation {
     private String lineName;
     private String startStation;
     private String endStation;
-    public LineInformation(String name){
-        this.lineName = name;
+    private LineType lineType;
+    private int value;
+    public LineInformation(List<String> information){
+        this.lineName = information.get(3);
+        this.startStation = information.get(0);
+        this.endStation = information.get(1);
+        if(information.get(4)=="시간"){
+            this.lineType = LineType.TIME;
+        }
+        if(information.get(4)=="거리"){
+            this.lineType = LineType.DISTANCE;
+        }
+       this.value = Integer.parseInt(information.get(2));
     }
 }
