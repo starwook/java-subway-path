4558f0cee653d56fabaf70a73535f626d36543e5
starwook <star.wookk@gmail.com>
2022-12-14 18:45:42 +0900

    feat: Add LineInformation.java

---
+++ src/main/java/subway/domain/LineInformation.java
@@ -0,0 +1,10 @@
+package subway.domain;
+
+public class LineInformation {
+    private String lineName;
+    private String startStation;
+    private String endStation;
+    public LineInformation(String name){
+        this.lineName = name;
+    }
+}
