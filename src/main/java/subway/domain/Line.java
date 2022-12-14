7b075add3c122bacca13d1644aa9cef7585405ad
starwook <star.wookk@gmail.com>
2022-12-14 17:41:35 +0900

    feat: Add init graph method

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,5 +1,11 @@
 package subway.domain;
 
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
     private String name;
 
@@ -12,4 +18,20 @@ public class Line {
     }
 
     // 추가 기능 구현
+    private WeightedMultigraph<String, DefaultWeightedEdge> timeGraph =
+            new WeightedMultigraph<>(DefaultWeightedEdge.class);
+    private WeightedMultigraph<String, DefaultWeightedEdge> distanceGraph =
+            new WeightedMultigraph<>(DefaultWeightedEdge.class);
+
+    public void addStationToGraph(String station){
+        timeGraph.addVertex(station);
+        distanceGraph.addVertex(station);
+    }
+    public void addEdgeDistance(String start,String end,int distance){
+        distanceGraph.setEdgeWeight(distanceGraph.addEdge(start,end),distance);
+    }
+    public void addEdgeTime(String start,String end,int time){
+        timeGraph.setEdgeWeight(timeGraph.addEdge(start,end),time);
+    }
+
 }
