7fc5d14d7c21b5a386ef9c88781e28fb415f10bb
starwook <star.wookk@gmail.com>
2022-12-15 12:02:14 +0900

    feat: Add WeightRepository.java

--- src/main/java/subway/domain/Weight.java
+++ src/main/java/subway/domain/Weight.java
@@ -7,7 +7,7 @@ public class Weight {
     private WeightedMultigraph<String, DefaultWeightedEdge> machine=
             new WeightedMultigraph<>(DefaultWeightedEdge.class);
 
-    public void addEdgeDistance(String start,String end,int value){
+    public void addEdgeValue(String start, String end, int value){
         machine.addVertex(start);
         machine.addVertex(end);
         machine.setEdgeWeight(machine.addEdge(start,end),value);
